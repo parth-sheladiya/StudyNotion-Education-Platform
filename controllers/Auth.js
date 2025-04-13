48003befcf31697a3a95e431a8c8eca7d6b34acf
parth sheladiya <parthsheladiya1427@gmail.com>
2025-04-13 10:03:32 +0530

    add signup code

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -45,6 +45,18 @@ exports.sendOTP = async (req, res) => {
 
       result = await OTP.findOne({ otp: otp });
     }
+
+    // otp payload 
+    const otpPayload = {email,otp};
+    // store otp in db
+    const otpBody = await OTP.create(otpPayload);
+    console.log("otp body", otpBody);
+
+    return res.status(200).json({
+      success:true,
+      message:"otp sent success fully",
+      otp
+    })
   } catch (error) {
     console.log("Error in generating OTP", error);
     return res.status(500).json({
@@ -53,3 +65,108 @@ exports.sendOTP = async (req, res) => {
     });
   }
 };
+
+
+// sign up 
+
+exports.signUp= async( req,res)=>{
+  try{
+    // data fetch from req. body 
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+    accountType,
+    contactNumber,
+    otp
+  } = req.body;
+
+  if(!firstName || !lastName || !email || !password || !confirmPassword || !otp){
+        return res.status(403).json({
+          success:false,
+          message:"all field are required"
+        })
+      }
+  // password and confirm password match 
+  if(password !== confirmPassword){
+    return res.status(400).json({
+      success:false,
+      message:"password and confirm password does not match "
+    })
+  }
+  
+  // check if user is exists or not
+
+  // check duplicate mail
+  const existingsUser = await User.findOne({email});
+  // if duplicate found 
+  if(existingsUser){
+    return res.status(400).json({
+      success:false,
+      message:"user already exists"
+    })
+  }
+
+  // find otp 
+  // createdAt:-1 means decending order sorting 
+  // if 1 means ascending order sorting 
+  const recentOTP =  await OTP.find({email}).sort({createdAt:-1}).limit(1);
+  console.log("recent otp is ", recentOTP);
+
+  // otp validation 
+  if(recentOTP.length ==0){
+    return res.status(400).json({
+      success:false,
+      message:"otp not found"
+    })
+  }
+
+  // match otp 
+  if(otp !== recentOTP.otp){
+    return res.status(400).json({
+      success:false,
+      message:"incorrect otp "
+    })
+  }
+
+  // password hash 
+  const hashedPassword = await bcrypt.hash(password,10);
+  console.log("hashed password is ", hashedPassword);
+
+  // store data to the data base 
+  // which data to store in additionaal details ??
+  const extraData = await  Profile.create({
+    gender:null,
+    dateOfBirth:null,
+    about:null,
+    contactNumber:null
+  })
+
+  // user model to store in database 
+  const user  = await User.create({
+    firstName,
+    lastName,
+    email,
+    contactNumber,
+    accountType,
+    additionalDetails:extraData._id,
+    image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
+  })
+
+  // return success mark 
+  return res.status(200).json({
+    success:true,
+    message:"user registered successfullt",
+    data:user
+  })
+  }
+  catch(error){
+    console.log("error while user register entry",error);
+    return res.status(500).json({
+      success:false,
+      message:"error in register user , !! please try again !!"
+    })
+  }
+}
